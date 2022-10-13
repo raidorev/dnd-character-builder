c556bdcdda801cc1b3dac70557daa0f8eb45d7fd
Alexander Titov <fox@raidorev.tech>
2022-10-13 17:35:55 +0600

    style(stylelint): add stylelint

--- apps/web/.lintstagedrc.js
+++ apps/web/.lintstagedrc.js
@@ -5,4 +5,5 @@ export default {
   // Lint all files because changes may affect other files
   '*.{js,jsx,ts,tsx,vue}': () => 'pnpm run lint',
   '**/*.vue': () => 'vue-tsc --noEmit',
+  '**/*.{vue,sass,scss}': 'stylelint',
 }
