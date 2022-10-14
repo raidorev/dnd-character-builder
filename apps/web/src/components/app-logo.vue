d9b9291a98ceb4d2b31d2b67f1f59b32547389cb
Alexander Titov <fox@raidorev.tech>
2022-10-14 23:31:19 +0600

    feat(web): [logo] add logo

---
+++ apps/web/src/components/app-logo.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+// https://github.com/import-js/eslint-plugin-import/issues/1739
+// eslint-disable-next-line import/no-unresolved
+import Logo from '../../public/logo.svg?component'
+
+withDefaults(defineProps<{ color: string }>(), {
+  color: '#fff',
+})
+</script>
+
+<template>
+  <Logo class="logo" viewBox="0 0 278 89" width="100" height="40" />
+</template>
+
+<style lang="scss" scoped>
+.logo :deep(path) {
+  fill: v-bind(color);
+}
+</style>
