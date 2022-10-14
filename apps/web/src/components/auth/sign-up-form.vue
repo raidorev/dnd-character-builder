35ad07a85e6006405a317e9a16e5419d297fe77d
Alexander Titov <fox@raidorev.tech>
2022-10-14 23:33:24 +0600

    feat(web): [auth] initial auth layout

---
+++ apps/web/src/components/auth/sign-up-form.vue
@@ -0,0 +1,38 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const email = ref('')
+const password = ref('')
+const passwordRepeat = ref('')
+
+const signUp = () => {
+  console.log(email.value, password.value)
+}
+</script>
+
+<template>
+  <v-form class="mb-3" @submit.prevent="signUp">
+    <v-text-field v-model="email" label="Email" type="email" required />
+    <v-text-field
+      v-model="password"
+      label="Password"
+      type="password"
+      required
+    />
+    <v-text-field
+      v-model="passwordRepeat"
+      label="Repeat password"
+      type="password"
+      required
+    />
+
+    <div class="text-center">
+      <v-btn class="mb-3" type="submit" color="primary ">Sign Up</v-btn>
+    </div>
+
+    <p class="text-subtitle-1 text-center">
+      Forgot your password?
+      <router-link to="/auth/forgot-password">Reset Password</router-link>
+    </p>
+  </v-form>
+</template>
--- apps/web/src/views/auth.vue
+++ apps/web/src/views/auth.vue
@@ -1,1 +1,99 @@
-<template>Auth</template>
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+import BaseLogo from '@/components/app-logo.vue'
+import SignInForm from '@/components/auth/sign-in-form.vue'
+import SignUpForm from '@/components/auth/sign-up-form.vue'
+
+const isSignUp = ref(false)
+
+const toggleSignUp = () => {
+  isSignUp.value = !isSignUp.value
+}
+</script>
+
+<template>
+  <v-row class="h-100" align="center" justify="center">
+    <v-col cols="12" sm="10" md="6" lg="6">
+      <v-sheet rounded elevation="5">
+        <v-row no-gutters>
+          <v-col cols="6" class="d-flex justify-center pa-3">
+            <div class="w-100">
+              <div class="d-flex align-center justify-center">
+                <BaseLogo color="rgb(var(--v-theme-primary))" />
+              </div>
+
+              <p class="text-subtitle-1 text-center mb-3">
+                Sign In to get access to some features
+              </p>
+
+              <v-scroll-x-transition mode="out-in">
+                <SignUpForm v-if="isSignUp" />
+                <SignInForm v-else />
+              </v-scroll-x-transition>
+
+              <p v-if="isSignUp" class="text-subtitle-1 text-center">
+                Already have an account?
+                <a href="#" role="button" @click.prevent="toggleSignUp">
+                  Sign In
+                </a>
+              </p>
+
+              <p v-else class="text-subtitle-1 text-center">
+                Don't have an account?
+                <a href="#" role="button" @click.prevent="toggleSignUp">
+                  Sign Up
+                </a>
+              </p>
+
+              <div class="horizontal-line-with-text my-3">
+                <div />
+                <span>Or</span>
+                <div />
+              </div>
+
+              <p class="text-subtitle-1 text-center mb-2">
+                Continue with social media
+              </p>
+              <div class="d-flex justify-center">
+                <v-btn
+                  class="mr-2"
+                  variant="flat"
+                  color="primary"
+                  icon="mdi-google"
+                />
+                <v-btn variant="flat" color="primary" icon="mdi-github" />
+              </div>
+            </div>
+          </v-col>
+          <v-col cols="6" class="d-none d-sm-block">
+            <v-img class="h-100" src="https://picsum.photos/1000" cover />
+          </v-col>
+        </v-row>
+      </v-sheet>
+    </v-col>
+  </v-row>
+</template>
+
+<style lang="scss" scoped>
+.horizontal-line-with-text {
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #9e9e9e;
+  text-align: center;
+  user-select: none;
+
+  div {
+    height: 1px;
+    flex: 1;
+    background-color: currentcolor;
+  }
+
+  span {
+    margin: 0 0.75rem;
+    font-size: 1rem;
+  }
+}
+</style>
