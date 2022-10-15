69de1bbbcad7aaad53c75a2236057b634dc9ef6f
Alexander Titov <fox@raidorev.tech>
2022-10-15 16:51:40 +0600

    style(eslint-config-custom): add prettier to the end of extends in ts files

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -78,6 +78,7 @@ module.exports = {
         'plugin:@typescript-eslint/recommended',
         'plugin:@typescript-eslint/recommended-requiring-type-checking',
         'plugin:@typescript-eslint/strict',
+        'plugin:prettier/recommended',
       ],
     },
     {
