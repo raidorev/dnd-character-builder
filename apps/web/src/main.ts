b3cfe29aae0e1654511d099828e2c224f628d16e
Alexander Titov <fox@raidorev.tech>
2022-10-15 16:53:30 +0600

    feat(web): add vue-i18n plugin

--- apps/web/src/main.ts
+++ apps/web/src/main.ts
@@ -3,6 +3,7 @@ import { createApp, h, provide } from 'vue'
 
 import App from './app.vue'
 import { apolloClient } from './plugins/apollo'
+import { i18n } from './plugins/i18n'
 import { pinia } from './plugins/pinia'
 import { router } from './plugins/router'
 import { vuetify } from './plugins/vuetify'
@@ -16,6 +17,7 @@ createApp({
   },
   render: () => h(App),
 })
+  .use(i18n)
   .use(vuetify)
   .use(pinia)
   .use(router)
