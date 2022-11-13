2c35c8c60de66bb8a51437502f119a0727b91482
Alexander Titov <fox@raidorev.tech>
2022-11-13 18:41:29 +0600

    feat(web): [auth] initial auth store implementation

--- apps/web/src/stores/auth.ts
+++ apps/web/src/stores/auth.ts
@@ -1,8 +1,57 @@
+import { provideApolloClient, useMutation } from '@vue/apollo-composable'
+import { gql } from 'graphql-tag'
 import { defineStore } from 'pinia'
-import { ref } from 'vue'
+import { computed, reactive, ref } from 'vue'
+
+import { apolloClient } from '@/plugins/apollo'
+import { extractErrorMessage } from '@/utils/graphql/error'
+
+export interface Token {
+  accessToken: string
+  refreshToken: string
+}
 
 export const useAuth = defineStore('auth', () => {
+  provideApolloClient(apolloClient)
+
+  const error = ref('')
+  const hasError = computed(() => error.value !== '')
+  const clearError = () => {
+    error.value = ''
+  }
+
+  // TODO
   const isSignedIn = ref(false)
 
-  return { isSignedIn }
+  const tokens = reactive({
+    accessToken: '',
+    refreshToken: '',
+  })
+
+  const {
+    mutate: signUp,
+    onError,
+    onDone,
+  } = useMutation<{ signUp: Token }>(gql`
+    mutation SignUp($email: String!, $password: String!) {
+      signUp(input: { email: $email, password: $password }) {
+        accessToken
+        refreshToken
+      }
+    }
+  `)
+
+  onError((apolloError) => {
+    error.value = extractErrorMessage(apolloError)
+  })
+
+  onDone((token) => {
+    clearError()
+    if (token.data) {
+      tokens.accessToken = token.data.signUp.accessToken
+      tokens.refreshToken = token.data.signUp.refreshToken
+    }
+  })
+
+  return { isSignedIn, signUp, error, hasError, clearError }
 })
